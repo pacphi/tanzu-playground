d39ea5e81af6fb5262879b901fb4ea07d3870c4b
Chris Phillipson <cphillipson@pivotal.io>
2020-05-11 20:00:24 -0700

    Update RBAC for networking in nginx-ingress

--- terraform/modules/dns/gcp/main.tf
+++ terraform/modules/dns/gcp/main.tf
@@ -12,7 +12,7 @@ resource "google_dns_record_set" "ns_record" {
   managed_zone = data.google_dns_managed_zone.root_zone.name
   name = "${var.dns_prefix}.${data.google_dns_managed_zone.root_zone.dns_name}"
   rrdatas = google_dns_managed_zone.zone.name_servers
-  
+
   ttl = 30
   type = "NS"
 
